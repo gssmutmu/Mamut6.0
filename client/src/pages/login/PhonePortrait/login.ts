c93b247e86837f0f223507a2d9cc850dfcb568b6
Muthuselvan Murugan <Muthuselvan.Murugan@flex.com>
2018-05-10 22:33:53 -0700

    [Studio] Modified screen "login"

--- client/src/pages/login/PhonePortrait/login.ts
+++ client/src/pages/login/PhonePortrait/login.ts
@@ -2,27 +2,28 @@ import { Component } from '@angular/core';
 import { Screen } from 'app/screen';
 
 @Component({
-  selector: 'screen-login-phoneportrait',
-  templateUrl: 'login.html'
+    selector: 'screen-login-phoneportrait',
+    templateUrl: 'login.html'
 })
 export class login_PhonePortrait extends Screen {
-  data: any;
+    data: any;
 
-  ngOnInit(): void {
-    super.ngOnInit();
-    // Logic to run when the screen loads goes here.
-  }
+    ngOnInit(): void {
+        super.ngOnInit();
+        // Logic to run when the screen loads goes here.
+    }
 
-  ngOnDestroy(): void {
-    super.ngOnDestroy();
-    // Logic to run when the screen unloads goes here.
-  }
+    ngOnDestroy(): void {
+        super.ngOnDestroy();
+        // Logic to run when the screen unloads goes here.
+    }
 
-  onDataLoad(data: any) {
-    // Logic to run when the screen's data is updated goes here.
-  }
-//   authaction()
-//   {
-//       console.log("chek");
-//     }
+    onDataLoad(data: any) {
+        // Logic to run when the screen's data is updated goes here.
+    }
+
+    public myAuthaction() {
+        console.log("chek");
+        authaction('submit');
+    }
 }
